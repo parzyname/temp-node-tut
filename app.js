7f2a70b9c99113c9b841e140d46679743728890a
parzy <pyrzadam3@gmail.com>
2022-02-04 05:57:43 +0000

    make a server

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const _ = require("lodash")
-const items = [1,2,[3,4,[2342,234,23],[23],[[[234,234,[123]]]]]]
-const newItems =_.flattenDeep(items)
-console.log(newItems)
+const express = require("express")
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/hello.txt")
+})
+
+app.listen(3000)
